999ce2fc0088151613a1d2508e1f2f5192aa57e9
jaywebwar <jaywebwar@gmail.com>
2018-09-22 22:10:47 -0400

    Full book prototype

---
+++ ApplePicker.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//necessary for text
+using UnityEngine.SceneManagement;//necessary for SceneManager
+
+public class ApplePicker : MonoBehaviour {
+
+    public GameObject       basketPrefab;
+    public int              numBaskets = 3;
+    public float            basketBottomY = -10f;
+    public float            basketSpacingY = 2f;
+    public Text             scoreUI;
+    private int             score;
+    public List<GameObject> basketList;
+    static private int      highScore = 10;
+    public Text             highScoreUI;
+
+    //Unity built-in function.  Always happens before Start.
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))//if it exists yet
+        {
+            highScore = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", highScore);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+		for(int i=0; i<numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+        score = 0;
+        setScore();
+        setHighScore();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AppleCaught()
+    {
+        score += 1;
+        setScore();
+        if(score > highScore)
+        {
+            highScore = score;
+            setHighScore();
+        }
+    }
+
+    void setScore()
+    {
+        scoreUI.text = "Apples Caught: " + score.ToString();
+    }
+
+    void setHighScore()
+    {
+        highScoreUI.text = "Most Apples Ever Caught: " + highScore.ToString();
+        PlayerPrefs.SetInt("ApplePickerHighScore", highScore);
+    }
+
+    public void AppleDestroyed()
+    {
+        //destroy all falling apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach(GameObject GO in tAppleArray)
+        {
+            Destroy(GO);
+        }
+
+        //destory a basket
+        //Get index of last basket in list
+        int basketIndex = basketList.Count - 1;//array starts at 0
+        GameObject basketToRemove = basketList[basketIndex];//get a reference to the GO
+        basketList.RemoveAt(basketIndex);//remove from list
+        Destroy(basketToRemove);//remove GO
+
+        //restart the game, when out of baskets
+        if(basketList.Count == 0)
+        {
+            //Application.LoadLevel("_Scene_0");
+            SceneManager.LoadScene("_Scene_0");
+        }
+    }
+}
---
+++ AppleTree.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+
+    //apple prefab
+    public GameObject applePrefab;
+
+    //AppleTree movement speed m/s
+    public float speed = 1f;
+
+    //Distance where tree turns around
+    public float leftAndRightEdge = 10f;
+
+    //chance of AppleTree changing directions
+    public float chanceToChangeDirections = 0.1f;
+
+    //rate at which apples are instantiated (seconds)
+    public float secondsBetweenAppleDrops = 1f;
+
+	// Use this for initialization
+	void Start () {
+        //Drop apples every second
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //Change Direction
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);//move left
+        }
+	}
+
+    //Called 50 times per second
+    void FixedUpdate()
+    {
+        //Changing Direction Randomly
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;//change direction
+        }
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+}
